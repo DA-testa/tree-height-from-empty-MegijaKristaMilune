db0bef2dee8bb4fc5a026717abe397ab14e27dd4
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-03-03 13:48:34 +0000

    test98765

--- tree_height.py
+++ tree_height.py
@@ -42,9 +42,6 @@ def main():
         parents = numpy.array(list(map(int, input().split())))
         print(compute_height(n, parents))
      
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27) 
 threading.Thread(target=main).start()

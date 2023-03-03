817175aafdccb070dcfb4d7d9819113cd7788b7b
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-03-03 13:48:53 +0000

    tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,46 +2,65 @@
 
 import sys
 import threading
-import numpy
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
 
-def compute_height(n, parents):
-    h = {}
+    def add_child(self, child):
+        self.children.append(child)
+        
+def compute_height(node):
+    # Write this function
+    max_height = 0
+    for child in node.children:
+        max_height = max(max_height, compute_height(child))
+    return max_height + 1
+
+def create_tree(n, mode, file=None):
+    parent_index=[]
+    nodes={}
+    if "I" in mode:
+        values = input().split()
+    else:
+        values = file.readline().split()
     for i in range(n):
-        if i not in h:
-            height = 1
-            thisnode = i
-            while parents[thisnode] != -1:
-                parent = parents[thisnode]
-                if parent not in h:
-                    thisnode = parent
-                    height = height + 1
-                else:
-                    height = height + h[parent]
-                    break
-            h[i] = height
-    return max(h.values())
-    
+        parent_index.append(int(values[i]))
+        nodes[i] = Node(i)
+          
+    for i in range(n):
+        if parent_index[i] != -1:
+            nodes[parent_index[i]].add_child(nodes[i])
+        
+        else:
+            root = nodes[i]
+
+    return root
+
 def main():
-    input_text = input()
-    if 'F' in input_text:
-        input_file = input()
-        input_file = "test/" + input_file
-        if 'a' not in input_file:
-            try:
-                with open(input_file, "r") as f:
-                    n = int(f.readline())
-                    parents = numpy.array(list(map(int, f.readline().split())))
-                    print(compute_height(n, parents))
-
-            except FileNotFoundError:
-                return print("error")
-
-    if 'I' in input_text:
+    print("Input Mode: ")
+    mode = input()
+    if "I" in mode:
+        print("Input: ")
         n = int(input())
-        parents = numpy.array(list(map(int, input().split())))
-        print(compute_height(n, parents))
-     
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27) 
+        root = create_tree(n, mode)
+        height = compute_height(root)
+        print(height)
+
+    else:
+        print("Input File: ")
+        filename= input()
+        if "a" in filename:
+            print("Filename containing a is not allowed")
+            return
+        folder = './test/'
+        file = open(folder + filename, 'r')
+        n = int(file.readline())
+        root = create_tree(n, mode, file)
+        height = compute_height(root)
+        print(height)
+        
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()

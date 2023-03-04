c9f4f25870f4d5d0aa0699f8bcac8cd59fc903f3
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-03-04 10:43:17 +0000

    test1

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,61 @@
-# python3
-
 import sys
 import threading
 import numpy
 
 
+
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    h = numpy.zeros(n)
+    maxh = -1
+
+
+    for i in range (len(parents)):
+        l = i
+        h_i = 1
+
+
+
+        while parents[l] != -1 :
+            if h[l] != 0 :
+                h_i += h[l] - 1
+                break
+            
+
+            h_i += 1
+
+            l = parents[l]
+
+        h[i] = h_i
+        maxh = max(maxh, h[i])
+    
+    return maxh
 
 
 def main():
-    # implement input form keyboard and from files
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    input_text = input()
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+    if 'F' in input_text:
+        input_file = input()
+        input_file = "test/" + input_file
+        if 'a' not in input_file:
+            
+            try:
+                with open(input_file, "r") as f:
+                    n = int(f.readline())
+                    parents = list(map(int, f.readline().split()))
+                    print(compute_height(n, parents))
+
+            except FileNotFoundError:
+                return print("not found")
+
+    if 'I' in input_text:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        print(compute_height(n, parents))
+
+
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
